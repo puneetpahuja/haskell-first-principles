fcd9cec93f715b7cd4444b060a9943ae22bf3843
Puneet Pahuja <er.puneetpahuja@gmail.com>
2017-09-13 16:34:24 +0530

    Complete ch2 exercises

--- ch2_exercises.hs
+++ ch2_exercises.hs
@@ -2,11 +2,11 @@
 
 module Ch2Exercises where
 
--- ### Parentheses and association
+-- # Parentheses and association
 -- same - 2
 -- different - 1, 3
 
--- ### Heal the sick
+-- # Heal the sick
 -- 1. Remove space between . and 1 in 3.14
 -- let area x = 3.14 * (x * x)
 
@@ -18,7 +18,7 @@ x = 7
 y = 10
 f = x + y
 
--- ## A head code
+-- # A head code
 -- 1. 5
 -- 2. 25
 -- 3. 30
@@ -31,5 +31,60 @@ xSquare = x * x
   where x = 5
 
 xy = x * y
-  where x = 6
+  where x = 5
         y = 6
+
+xPlus3 = x + 3
+  where x = 3
+        y = 1000
+
+first = x * 3 + y
+  where x = 3
+        y = 1000
+
+second = x * 5
+  where y = 10
+        x = 10 * 5 + y
+
+third = z / x + y
+  where x = 7
+        y = negate x
+        z = y * 10
+
+-- # Parenthesization
+eEx = 2 + 2 * 3 - 3             -- 5
+eExS = 2 + (2 * 3) - 3          -- 5   -- `e` stands for expression, `S` stands for Solution
+
+e1 = 2 + 2 * 3 - 1              -- 7
+e1S = 2 + (2 * 3) - 1           -- 7
+
+e2 = (^) 10 $ 1 + 1             -- 100
+e2S = (^) 10 (1 + 1)            -- 100
+e2S2 = (^) 10 $ (1 + 1)         -- 100
+
+e3 = 2 ^ 2 * 4 ^ 5 + 1          -- 4097
+e3S = ((2 ^ 2) * (4 ^ 5)) + 1   -- 4097
+
+-- # Equivalent expressions
+-- Answer - 1, 2
+-- 4 - div is integral division
+
+-- # More fun with functions
+zz = 7
+yy = zz + 8                     -- 15
+xx = yy ^ 2                     -- 225
+waxOn = xx * 5                  -- 1125
+
+-- 1. 1135, 1135, -1110, 1110
+
+-- 4.
+waxOn2 = x * 5
+  where z = 7
+        x = y ^ 2
+        y = z + 8
+
+-- 5.
+triple x = x * 3
+
+-- 6.
+waxOff x = triple x
